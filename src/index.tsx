70ad93c0073ddef788d094e85fe20108746c4c98
andre <andrefer483@gmail.com>
2022-04-11 02:47:28 -0300

    Updated react/react-dom versions and global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import GlobalStyles from './styles/global'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
 
+if (container) {
+  createRoot(container).render(
+    <React.StrictMode>
+      <GlobalStyles />
+      <App />
+    </React.StrictMode>
+  );
+}
